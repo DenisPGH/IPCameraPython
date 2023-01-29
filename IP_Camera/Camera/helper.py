1e7f9526331424eb6643946ce4a8ab8be2cb0c9f
Denislav <deni_9000_1990@abv.bg>
2023-01-29 08:35:48 +0100

    setup django

---
+++ IP_Camera/Camera/helper.py
@@ -0,0 +1,52 @@
+
+import cv2
+import threading
+
+
+
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        try:
+            while True:
+                (self.grabbed, self.frame) = self.video.read()
+                if not self.grabbed:
+                    break
+                    #print("not grabbed")
+        except:
+            #print("break update")
+            self.video.release()
+            cv2.destroyAllWindows()
+
+    def stop(self):
+        return self.video.release()
+
+def gen(camera):
+    while True:
+        try:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'
+                   )
+        except:
+            #print("break in gen cam")
+            VideoCamera.stop(camera)
+            cv2.destroyAllWindows()
+            break
+
+
+
---
+++ IP_Camera/Camera/views.py
@@ -0,0 +1,26 @@
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+from  django.views import generic as view_dj
+from Camera.helper import VideoCamera, gen
+from django.views.decorators import gzip
+
+
+class IndexView(view_dj.TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['start'] = True
+        return context
+
+
+
+
+@gzip.gzip_page
+def openCam(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+    return render(request, 'stream.html')
