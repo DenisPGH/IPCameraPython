5e304e46ea22be3ba26babb103d9152175fb591f
Denislav <deni_9000_1990@abv.bg>
2023-01-29 17:05:19 +0100

    experiments 0

--- IP_Camera/test_camera.py
+++ IP_Camera/test_camera.py
@@ -22,8 +22,11 @@
 import cv2
 
 vid = cv2.VideoCapture(0)
+if not vid.isOpened():
+    print('is not open')
+    vid.open()
 
-while (True):
+while True:
     ret, frame = vid.read()
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
